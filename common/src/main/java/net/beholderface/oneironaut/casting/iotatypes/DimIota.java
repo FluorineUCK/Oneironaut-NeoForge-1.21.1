ad811dca7b3c51669be54dec63fc812c521725fe
beholderface <13290805+beholderface@users.noreply.github.com>
2025-11-26 02:32:41 -0500

    refactor imprint display stuff
    it is now possible for other mods to register their own colors and styles and stuff for their dimensions' imprints

--- common/src/main/java/net/beholderface/oneironaut/casting/iotatypes/DimIota.java
+++ common/src/main/java/net/beholderface/oneironaut/casting/iotatypes/DimIota.java
@@ -6,20 +6,25 @@ import at.petrak.hexcasting.api.utils.HexUtils;
 import dev.architectury.platform.Platform;
 import dev.architectury.utils.Env;
 import net.beholderface.oneironaut.MiscClientAPIKt;
+import net.beholderface.oneironaut.Oneironaut;
 import net.beholderface.oneironaut.registry.OneironautIotaTypeRegistry;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.nbt.NbtElement;
 import net.minecraft.registry.RegistryKey;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
 import net.minecraft.server.world.ServerWorld;
-import net.minecraft.text.*;
 
-//import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.BiFunction;
 
 public class DimIota extends Iota {
     public DimIota(@NotNull String dim){
@@ -67,6 +72,19 @@ public class DimIota extends Iota {
         data.putString(DIM_KEY, (String) payload);
         return data;
     }
+
+    private static final Map<UUID, TextTransformer> styleTransformers = new HashMap<>();
+
+    public static void registerTransformer(TextTransformer transformer){
+        styleTransformers.put(transformer.uuid, transformer);
+    }
+    public static TextTransformer getTransformer(UUID uuid){
+        return styleTransformers.get(uuid);
+    }
+    public static void removeTransformer(UUID uuid){
+        styleTransformers.remove(uuid);
+    }
+
     public static IotaType<DimIota> TYPE = new IotaType<>() {
         @Override
         public DimIota deserialize(NbtElement tag, ServerWorld world) throws IllegalArgumentException {
@@ -74,20 +92,41 @@ public class DimIota extends Iota {
             return new DimIota(ctag.getString(DIM_KEY));
         }
 
+        static {
+            registerTransformer(TextTransformer.colorizer("minecraft:overworld", 0x00aa00));
+            registerTransformer(TextTransformer.colorizer("minecraft:the_nether", 0xaa0000));
+            registerTransformer(TextTransformer.colorizer("minecraft:the_end", 0xffff55));
+            registerTransformer(new TextTransformer(UUID.randomUUID(), (t, s)->{
+                if (s.equals("oneironaut:noosphere")){
+                    return t.copy().setStyle(t.getStyle().withColor(0xaa00aa).withBold(true));
+                }
+                return t;
+            }));
+            registerTransformer(new TextTransformer(UUID.randomUUID(), (t, s)->{
+                if (s.equals("oneironaut:deep_noosphere")){
+                    return t.copy().setStyle(randomizedFormatting(t.getStyle().withColor(0xb300de)));
+                }
+                return t;
+            }));
+        }
+
         @Override
         public Text display(NbtElement tag) {
             var ctag = HexUtils.downcast(tag, NbtCompound.TYPE);
-            var text = Text.of(ctag.getString(DIM_KEY));
-            Style originalStyle = text.getStyle();
-            Style formattedStyle = switch (text.getString()) {
-                case "minecraft:overworld" -> originalStyle.withColor(0x00aa00);
-                case "minecraft:the_nether" -> originalStyle.withColor(0xaa0000);
-                case "minecraft:the_end" -> originalStyle.withColor(0xffff55);
-                case "oneironaut:noosphere" -> originalStyle.withColor(0xaa00aa).withBold(true);
-                case "oneironaut:deep_noosphere" -> randomizedFormatting(originalStyle.withColor(0xb300de));
-                default -> originalStyle.withColor(0x5555ff);
-            };
-            return text.copy().setStyle(formattedStyle);
+            String worldKey = ctag.getString(DIM_KEY);
+            Text text = Text.of(worldKey).copy();
+            String originalString = text.getString();
+            text = text.copy().setStyle(text.getStyle().withColor(0x5555ff)); //default coloring
+            for (TextTransformer transformer : styleTransformers.values()){
+                text = transformer.transform(text, worldKey);
+            }
+            //don't let people change the actual string of the text server-side, it might break hexes that rely on stuff like Scrivener's
+            if (Oneironaut.getCachedServer() != null && Thread.currentThread() == Oneironaut.getCachedServer().getThread()
+                    && !text.getString().equals(originalString)){
+                text = Text.of(worldKey).copy().setStyle(text.getStyle());
+            }
+
+            return text;
         }
 
         private static Style randomizedFormatting(Style original){
@@ -130,4 +169,27 @@ public class DimIota extends Iota {
             return 0xff_5555FF;
         }
     };
+
+    public static class TextTransformer {
+        public final UUID uuid;
+        protected final BiFunction<Text, String, Text> function;
+        public TextTransformer(UUID uuid, BiFunction<Text, String, Text> function){
+            this.uuid = uuid;
+            this.function = function;
+        }
+        public Text transform(Text text, String worldKey){
+            return function.apply(text, worldKey);
+        }
+
+        public static TextTransformer colorizer(String keyToColorize, int color){
+            BiFunction<Text, String, Text> transformer = (original, string) ->{
+                if (string.equals(keyToColorize)){
+                    return original.copy().setStyle(original.getStyle().withColor(color));
+                } else {
+                    return original;
+                }
+            };
+            return new TextTransformer(UUID.randomUUID(), transformer);
+        }
+    }
 }
